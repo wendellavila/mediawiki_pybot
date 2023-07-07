9b88c4aeabb98e8b44b0b833dd5c9f8eade6bcc8
wendellavila <wendelljcavila@gmail.com>
2023-07-07 19:16:20 -0300

    refactor: mw_functions renamed to libmediawiki; utils.py created with functions that only deal with local files; file treatment moved from main sh file to utils and libmediawiki

--- mediawiki-pybot.sh
+++ mediawiki-pybot.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import argparse
-import mw_api_functions
+import libmediawiki
 import json
 import os
 import sys
+import utils
 
 parser = argparse.ArgumentParser(
     prog='mediawiki-pybot',
@@ -52,7 +53,7 @@ parser_edit.add_argument('-s', '--substitution', action='store',
     help="path to a text file containing a list of text/regex substitutions to be applied when editing pages. See substitution_example.txt for usage.")
 parser_edit.add_argument('-a', '--append', action='store', help="string to be appended to pages when editing")
 parser_edit.add_argument('-p', '--prepend', action='store', help="string to be prepended to pages when editing")
-parser_edit.add_argument('--summary', nargs='?', const="", action='store', help="edit summary")
+parser_edit.add_argument('--summary', action='store', help="edit summary")
 parser_edit.add_argument('--pagelist-path', action='store', help="loads a pagelist file from a custom location")
 parser_edit.add_argument('--skip-if', action='store', help="pages that contain given string or regex won't be edited")
 parser_edit.add_argument('--skip-ifnot', action='store', help="pages that doesn't contain given string or regex won't be edited")
@@ -61,9 +62,8 @@ parser_edit.add_argument('-d', '--delay', action='store', help="delay between ea
 parser_create = subparsers.add_parser('create', help="mass create pages. for options see 'mediawiki-pybot create --help'.")
 parser_create.add_argument('-c', '--content', action='store', help="content to be added to each page", required=True)
 parser_create.add_argument('-p', '--pagelist-path', action='store', help="loads a pagelist file from a custom location")
-parser_create.add_argument('-s', '--summary', nargs='?', const="", action='store', help="edit summary")
+parser_create.add_argument('-s', '--summary', action='store', help="edit summary")
 parser_create.add_argument('-d', '--delay', action='store', help="delay between each edit, in seconds", type=int)
-parser_create.add_argument('--create-only', action='store', nargs='?', const=True, default=True, help="whether or not to skip the page if it already exists. default= True", type=bool)
 
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
@@ -80,72 +80,29 @@ if len(sys.argv)==2:
 else:
     #performing actions based on args
     if args.operation == "save":
-        if os.path.exists("credentials.json"):
-            with open('credentials.json') as input_file:
-                credentials = json.load(input_file)
-        else:
-            credentials = {'username':None,'password':None,'url':None}
-
-        credentials['username'] = args.username if args.username is not None else credentials['username']
-        credentials['password'] = args.password if args.password is not None else credentials['password']
-        credentials['url'] = args.url if args.url is not None else credentials['url']
-
-        with open("credentials.json", "w") as output_file:
-            json.dump(credentials, output_file)
+        utils.save_credentials("credentials.json", username=args.username, password=args.password, url=args.url)
         print("Credentials saved successfully.")
     elif args.operation == "pagelist":
-        pagelist_path = args.save_path if args.save_path is not None else "pagelist.txt"
-        pagelist_mode = "a" if args.no_reset else "w"
-            
-        if os.path.exists("credentials.json"):
-            with open('credentials.json') as credentials_file:
-                credentials = json.load(credentials_file)
-                url = credentials['url']
+        PAGELIST_PATH = args.save_path if args.save_path is not None else "pagelist.txt"
+        PAGELIST_MODE = "a" if args.no_reset else "w"
+        URL = libmediawiki.get_url("credentials.json")
+        pagelist = libmediawiki.generate_pagelist(url=URL, pagelist_source=args.source, pagelist_target=args.target,
+        namespace=args.namespace, limit=args.limit)
+        if len(pagelist) == 0:
+            print("No pages found with given source and target.")
         else:
-            raise Exception("Unable to get pages: No saved url. Run 'mediawiki-pybot save' to save credentials.")
-        with open(pagelist_path, pagelist_mode) as pagelist_file:
-            pagelist = mw_api_functions.get_pagelist(url=url, pagelist_source=args.source, pagelist_target=args.target,
-            namespace=args.namespace, limit=args.limit)
-            for pagename in pagelist:
-                pagelist_file.write("{}\n".format(pagename))
-            if len(pagelist) == 0:
-                print("No pages found with given source and target.")
-            else:
-                print(f"{args.source.capitalize()}:{args.target} - {len(pagelist)} pages added to pagelist.")
+            utils.write_pagelist(pagelist, PAGELIST_PATH, PAGELIST_MODE)
+            print(f"{args.source.capitalize()}:{args.target} - {len(pagelist)} pages added to pagelist.")
     elif args.operation == "edit":
-        if os.path.exists("credentials.json"):
-            with open('credentials.json') as credentials_file:
-                credentials = json.load(credentials_file)
-            if credentials['username'] == None or credentials['password'] == None or credentials['url'] == None:
-                raise Exception("Unable to login: Saved credentials partially missing. Run 'mediawiki-pybot save' to save credentials.")
-            else:
-                CSRF_TOKEN = mw_api_functions.login(username=credentials['username'], password=credentials['password'], url=credentials['url'])
-        else:
-            raise Exception("Unable to login: No saved credentials. Run 'mediawiki-pybot save' to save credentials.")
-        
+        CSRF_TOKEN = libmediawiki.get_token("credentials.json")
+        URL = libmediawiki.get_url("credentials.json")
         pagelist_path = args.pagelist_path if args.pagelist_path is not None else "pagelist.txt"
-        pagelist = []
-        with open(pagelist_path, "r") as pagelist_file:
-            for line in pagelist_file.readlines():
-                pagelist.append(line)
-        mw_api_functions.edit_pages(csrf_token=CSRF_TOKEN, url=credentials['url'], pagelist=pagelist, summary=args.summary, substitution_path=args.substitution, append=args.append,
+        libmediawiki.edit_pages(csrf_token=CSRF_TOKEN, url=URL, pagelist_path=pagelist_path, summary=args.summary, substitution_path=args.substitution, append=args.append,
         prepend=args.prepend, skip_if=args.skip_if, skip_ifnot=args.skip_ifnot, delay=args.delay)
     elif args.operation == "create":
-        if os.path.exists("credentials.json"):
-            with open('credentials.json') as credentials_file:
-                credentials = json.load(credentials_file)
-            if credentials['username'] == None or credentials['password'] == None or credentials['url'] == None:
-                raise Exception("Unable to login: Saved credentials partially missing. Run 'mediawiki-pybot save' to save credentials.")
-            else:
-                CSRF_TOKEN = mw_api_functions.login(username=credentials['username'], password=credentials['password'], url=credentials['url'])
-        else:
-            raise Exception("Unable to login: No saved credentials. Run 'mediawiki-pybot save' to save credentials.")
-
+        CSRF_TOKEN = libmediawiki.get_token("credentials.json")
+        URL = libmediawiki.get_url("credentials.json")
         pagelist_path = args.pagelist_path if args.pagelist_path is not None else "pagelist.txt"
-        pagelist = []
-        with open(pagelist_path, "r") as pagelist_file:
-            for line in pagelist_file.readlines():
-                pagelist.append(line)
-        mw_api_functions.create_pages(csrf_token=CSRF_TOKEN, url=credentials['url'], content=args.content, pagelist=pagelist, summary=args.summary, delay=args.delay, create_only=args.create_only)
+        libmediawiki.create_pages(csrf_token=CSRF_TOKEN, url=URL, content=args.content, pagelist_path=pagelist_path, summary=args.summary, delay=args.delay)
         
 parser.exit()
